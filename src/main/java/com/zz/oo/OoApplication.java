d4520f0e1210aef11c1f97e6e3526b3f8303fe9b
yanggang <31473199@qq.com>
2018-09-15 11:21:01 +0800

    OO Spring Boot Start

---
+++ src/main/java/com/zz/oo/OoApplication.java
@@ -0,0 +1,12 @@
+package com.zz.oo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OoApplication.class, args);
+    }
+}
